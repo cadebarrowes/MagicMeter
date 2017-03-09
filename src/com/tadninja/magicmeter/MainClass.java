a4f2ce96a3f1d5df4b269636a8ee21f1c921da46
cbar0164 <cbar0164@711S-SC110-17.canyonsdistrict.pvt>
2017-03-09 08:08:56 -0700

    3rd commit I think? I might have changed some stuff idk

--- src/com/tadninja/magicmeter/MainClass.java
+++ src/com/tadninja/magicmeter/MainClass.java
@@ -2,8 +2,17 @@ package com.tadninja.magicmeter;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
 import com.tadninja.magicmeter.commands.MagicMeter;
 
 public class MainClass extends JavaPlugin {
@@ -33,5 +42,21 @@ public class MainClass extends JavaPlugin {
 		logger.info(pdfFile.getName() + " has been disabled (V." + pdfFile.getVersion() + ")");
 
 	}
-	
+	// https://bukkit.org/threads/right-click-with-item-event.73590/
+	@SuppressWarnings("deprecation")
+	@EventHandler(priority=EventPriority.HIGH)
+	public void onPlayerUse(PlayerInteractEvent event){
+	    Player p = event.getPlayer();
+	 
+	    if(event.getAction().equals(Action.RIGHT_CLICK_AIR)){
+	        if(p.getItemInHand().getType() == Material.BLAZE_POWDER){
+	            Fireball fire = p.getWorld().spawn(event.getPlayer().getLocation().add(new Vector(0.0D, 1.0D, 0.0D)), Fireball.class);
+	            fire.setFireTicks(0);
+	            fire.setShooter(p);
+	        }
+	        else if(p.getItemInHand().getType() == Material.BLAZE_ROD){
+	            //Do whatever
+	        }
+	    }
+	}
 }
--- src/com/tadninja/magicmeter/commands/MagicMeter.java
+++ src/com/tadninja/magicmeter/commands/MagicMeter.java
@@ -25,7 +25,11 @@ public class MagicMeter implements CommandExecutor, Listener {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 		
 	}
-
+	/*
+	 * 
+	 * 
+	 * 
+	 * */
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
 		// sender is the one initiating the command, label is what they typed
@@ -41,71 +45,78 @@ public class MagicMeter implements CommandExecutor, Listener {
 		/* sets offhand to golden hoe with durability to match meter */
 		Player player = (Player) sender;
 
-		/* FOR THE METADATA -1 = off, 0 is empty and 16 is full */
+		/* FOR THE METADATA -1 = off, 0 is empty and 64 is full */
 		
 		//they typed /mm and something else
 		if (args.length == 1) { 
 			
 			// if they typed /mm start 
 			if (args[0].equals("start")) {
-				
-				// if they don't have a value for Magic Amount
-				if (!(player.hasMetadata("Magic Amount"))) { 
-					
-					player.sendMessage(ChatColor.GREEN + "Starting MagicMeter");
-					
-					// set their value to zero so it will be added to every tick
-					player.setMetadata("Magic Amount", new FixedMetadataValue(mainPlugin, 0));
-					
-					return true; //stahp here we done
-					
-				} 
-				// if it comes this far we know they have a value for Magic Amount
-				
-				if (player.getMetadata("Magic Amount").get(0).asInt()==(-1)) {
-					// or if it is -1 set their value to zero so it will be added to every tick
-					player.setMetadata("Magic Amount", new FixedMetadataValue(mainPlugin, 0));
-					
-					return true;
-				}
-				// if it comes this far we know Magic Meter is still going
-				
-				player.sendMessage(ChatColor.GRAY + "Magic Meter is still going!");
-				
-				return false;
+				return start();
 			}
 			// if they typed /mm stahp 
 			if (args[0].equals("stop")) {
-				
-				// if they don't have a value for Magic Amount
-				if (!(player.hasMetadata("Magic Amount"))) { 
-					
-					player.sendMessage(ChatColor.GRAY + "You haven't started Magic Meter yet!");
-					
-					return false; //stahp here we done
-					
-				} 
-				// if it comes this far we know they have a value for Magic Amount
-				
-				if (player.getMetadata("Magic Amount").get(0).asInt()==(-1)) {
-					
-					player.sendMessage(ChatColor.GRAY + "You haven't started Magic Meter yet!");
-					
-					return false; //stahp here we done
-					
-				}
-				// if it comes to this point we know Magic Meter is going
-				
-				player.sendMessage(ChatColor.RED + "Stopping MagicMeter");
-				
-				player.setMetadata("Magic Amount", new FixedMetadataValue(mainPlugin, -1));
-				
-				player.getInventory().setItemInOffHand(new ItemStack(Material.AIR)); //remove golden hoe
-				
-				return true;
+				return stop();
 			}
 		}
 		return false;
+	}
+	private boolean start() {
+		
+		// if they don't have a value for Magic Amount
+		if (!(player.hasMetadata("Magic Amount"))) { 
+			
+			player.sendMessage(ChatColor.GREEN + "Starting MagicMeter");
+			
+			// set their value to zero so it will be added to every tick
+			player.setMetadata("Magic Amount", new FixedMetadataValue(mainPlugin, 0));
+			
+			return true; //stahp here we done
+			
+		} 
+		// if it comes this far we know they have a value for Magic Amount
+		
+		if (player.getMetadata("Magic Amount").get(0).asInt()==(-1)) {
+			// or if it is -1 set their value to zero so it will be added to every tick
+			player.setMetadata("Magic Amount", new FixedMetadataValue(mainPlugin, 0));
+			
+			return true;
+		}
+		// if it comes this far we know Magic Meter is still going
+		
+		player.sendMessage(ChatColor.GRAY + "Magic Meter is still going!");
+		
+		return false;
+		
+	}
+	private boolean stop() {
+		
+		// if they don't have a value for Magic Amount
+		if (!(player.hasMetadata("Magic Amount"))) { 
+			
+			player.sendMessage(ChatColor.GRAY + "You haven't started Magic Meter yet!");
+			
+			return false; //stahp here we done
+			
+		} 
+		// if it comes this far we know they have a value for Magic Amount
+		
+		if (player.getMetadata("Magic Amount").get(0).asInt()==(-1)) {
+			
+			player.sendMessage(ChatColor.GRAY + "You haven't started Magic Meter yet!");
+			
+			return false; //stahp here we done
+			
+		}
+		// if it comes to this point we know Magic Meter is going
+		
+		player.sendMessage(ChatColor.RED + "Stopping MagicMeter");
+		
+		player.setMetadata("Magic Amount", new FixedMetadataValue(mainPlugin, -1));
+		
+		player.getInventory().setItemInOffHand(new ItemStack(Material.AIR)); //remove golden hoe
+		
+		return true;
 	}
 
 }
